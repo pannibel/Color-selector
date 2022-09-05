4349cc97cea23cfa07be355d7d20182be486ad11
Panni Belevski <pannibel@gmail.com>
2022-09-05 21:54:13 +0200

    convert to rgb working

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 "use strict"
 
 // SETTING UP
+let color;
+let hexCode;
+let rgbCode;
+let hslCode;
+
 let colorInput = document.getElementById("color_picker");
 
 // EVENTLISTENERS
-colorInput.addEventListener("input", () => {
-    console.log(colorInput.value);
-});
+colorInput.addEventListener("input", colorDisplay);
 
 
 //MAKING RGB, HEX AND HSL CODES
-function makeRGB() {
-
+function makeHEX() {
+    hexCode = colorInput.value;
 }
 
-function makeHEX() {
+function makeRGB(color) {
 
+    color = colorInput.value;
+    let r = parseInt(color.substr(1, 2), 16);
+    let g = parseInt(color.substr(3, 2), 16);
+    let b = parseInt(color.substr(5, 2), 16);
+
+    rgbCode = r + ", " + g + ", " + b;
 }
 
 function makeHSL() {
 
+}
+
+//DISPLAYING THE COLOR AND THE CODES
+function colorDisplay() {
+    makeHEX(color);
+    makeRGB(color);
+
+    console.log(hexCode);
+    console.log(rgbCode);
 }
