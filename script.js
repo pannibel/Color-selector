c70f6ae40d95e67473e2507276751d34837186b3
Panni Belevski <pannibel@gmail.com>
2022-09-05 21:39:44 +0200

    color input working

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 "use strict"
 
+// SETTING UP
+let colorInput = document.getElementById("color_picker");
+
+// EVENTLISTENERS
+colorInput.addEventListener("input", () => {
+    console.log(colorInput.value);
+});
+
+
+//MAKING RGB, HEX AND HSL CODES
+function makeRGB() {
+
+}
+
+function makeHEX() {
+
+}
+
+function makeHSL() {
+
+}
